e20fc0f4cd72d660944c99e49c1544578bc8af09
Akram <akram.louati@esprit.tn>
2023-02-23 03:39:53 +0100

    Update DonType.php

--- src/Form/DonType.php
+++ src/Form/DonType.php
@@ -6,18 +6,18 @@ use App\Entity\Don;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 class DonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('montant', TextType::class, [
-                'attr' => ['class' => 'form-control']
-            ])
+            ->add('montant')
             ->add('date_Don')
-          //  ->add('id_Membre')
-          //  ->add('id_Assoc')
+            ->add('carteCredit')
+            ->add('message')
+            ->add('Membre')
+            ->add('Association')
         ;
     }
 
